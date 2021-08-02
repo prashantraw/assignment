5b92eb5faeafcb5d96cd245ec4de776b5b1cffc0
Prashant Rawat <prashant008.pr@gmail.com>
2021-08-02 16:31:48 +0530

    to main

--- file1.js
+++ file1.js
@@ -1,4 +1,24 @@
-hello
-world
-test
-ocean
+function salesTax(x) {
+    let totalTax = 0
+    let tax = 0.1
+    let importDuty = 0
+    let noTax = ['book', 'chocolate', 'pill']
+    if (x.includes("imported")) {
+        importDuty = 0.05;
+    }
+
+    const n = x.split(" ");
+    const isTrue = noTax.find(c => n.includes(c))
+    if (isTrue) {
+        tax = 0;
+       }
+    
+    totalTax = tax + importDuty
+    const price = parseFloat(n[n.length - 1])
+    const finalTax = price * totalTax
+
+    return (finalTax + price);
+
+}
+const a = salesTax('1 imported chocolate at 11.25')
+console.log(a)
